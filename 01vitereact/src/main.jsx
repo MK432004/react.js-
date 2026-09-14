ee95bd7dc43e855e37bb2c22cbe84bed6b004025
Manishkumar <manish089808@gmail.com>
2026-09-14 09:16:41 +0530

    error solve

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactDOM from  "react-dom/clicent"
+import ReactDOM from  "react-dom/client"
 import App from "./App.jsx"
 
 
@@ -7,5 +7,6 @@ import App from "./App.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).
 render(
+    
     <App />
 )
