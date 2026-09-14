cf111c9451b1410a92fc3b35365fbdaa061518bd
Manishkumar <manish089808@gmail.com>
2026-09-14 08:48:49 +0530

    understand file structure and print helo react js

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,4 +1,11 @@
-import React from "react";
+import React from "react"
 import ReactDOM from  "react-dom/clicent"
 import App from "./App.jsx"
 
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+    <App />
+)
