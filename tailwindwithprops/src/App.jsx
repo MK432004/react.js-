6b74355e88ae9aa3cf4344156f6adef8f52802ea
Manishkumar <manish089808@gmail.com>
2026-09-18 14:03:32 +0530

    create a components

--- tailwindwithprops/src/App.jsx
+++ tailwindwithprops/src/App.jsx
@@ -1,15 +1,20 @@
+
+
 import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Card from './Componentes/Card';
 
 function App() {
  const[counter,setCounter] = useState(0)
 
   return (
     <>
-    <h >Tailwind test </h>
+    <h className='bg-green-400 rounded text-2xl text-black' >Tailwind test </h>
+    <Card/> <Card/>
+
     </>
   )
 }
